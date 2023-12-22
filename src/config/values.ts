eeb0d3f4168858c9c46b0b9115225e2ff1ff6399
keneanung <keneanung@googlemail.com>
2023-12-22 22:48:41 +0000

    Fix fetch

--- src/config/values.ts
+++ src/config/values.ts
@@ -5,6 +5,6 @@ dotenv.config();
 
 export const config = { 
     port: process.env.PORT || 3000,
-    mapFile: process.env.MAP_FILE || path.join(process.cwd(), "map.json"),
-    mapDownloadUrl: process.env.MAP_DOWNLOAD_URL || "https://ire-mudlet-mapping.github.io/AchaeaCrowdmap/Maps/map",
+    mapFile: process.env.MAP_FILE || path.join(process.cwd(), "map"),
+    mapDownloadUrl: process.env.MAP_DOWNLOAD_URL || "https://ire-mudlet-mapping.github.io/AchaeaCrowdmap/Map/map",
 }
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import { app } from "./app";
 import { config } from "./config/values";
-import fetch from "node-fetch";
 import * as fs from "fs";
+import { Readable } from "stream";
 
 if (!fs.existsSync(config.mapFile)) {
   fetch(config.mapDownloadUrl).then((res) => {
     const fileStream = fs.createWriteStream(config.mapFile);
     new Promise((resolve, reject) => {
-      res.body?.on("error", reject);
-      res.body?.pipe(fileStream);
+      if(res.body === null){
+        throw Error("No body")
+      }
+      const readable = Readable.fromWeb(res.body)
+      readable.on("error", reject);
+      readable.pipe(fileStream);
       fileStream.on("finish", resolve);
     });
   });
