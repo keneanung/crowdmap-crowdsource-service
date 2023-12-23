2e36dbd20a07d0f39db240ed54e9b83ece9db39b
keneanung <keneanung@googlemail.com>
2023-12-23 23:12:58 +0000

    Fix some linting errors

--- src/config/values.ts
+++ src/config/values.ts
@@ -4,9 +4,9 @@ import * as path from "path";
 dotenv.config();
 
 export const config = {
-  port: process.env.PORT || 3000,
-  mapFile: process.env.MAP_FILE || path.join(process.cwd(), "map"),
+  port: process.env.PORT ?? 3000,
+  mapFile: process.env.MAP_FILE ?? path.join(process.cwd(), "map"),
   mapDownloadUrl:
-    process.env.MAP_DOWNLOAD_URL ||
+    process.env.MAP_DOWNLOAD_URL ??
     "https://ire-mudlet-mapping.github.io/AchaeaCrowdmap/Map/map",
 };
--- src/controllers/mapController.ts
+++ src/controllers/mapController.ts
@@ -8,13 +8,17 @@ import { provideSingleton } from "../ioc/provideSingleton";
 export class MapController extends Controller {
   @Get("/")
   @Produces("application/octet-stream")
-  public async getMap(): Promise<Readable> {
+  public getMap(): Readable {
     this.setHeader("Content-Type", "application/octet-stream");
     this.setHeader("Content-Disposition", "attachment; filename=map.html");
 
     const s = fs.createReadStream(__dirname + "/index.html");
     s.on("close", () => {
-      fs.unlink(__dirname + "/index.html", () => {});
+      fs.unlink(__dirname + "/index.html", (err) => {
+        if(err){
+            throw err;
+        }
+      });
     });
     return s;
   }
