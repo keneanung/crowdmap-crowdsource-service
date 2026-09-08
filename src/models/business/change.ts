b88fb35ab4e38f20677690510364cf3181a76117
keneanung <keneanung@googlemail.com>
2026-09-08 17:06:01 +0200

    Ignore room hash changes for missing rooms

--- src/models/business/change.ts
+++ src/models/business/change.ts
@@ -573,6 +573,8 @@ export class SetRoomHash extends RoomChangeBase<SetRoomHash> {
   }
 
   public apply(map: Mudlet.MudletMap): void {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (!map.rooms[this.roomNumber]) return;
     for (const [hash, roomNumber] of Object.entries(
       map.mpRoomDbHashToRoomId,
     )) {
